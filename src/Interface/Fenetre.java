e70da3fb85306e03ab5c6f7234df54912c823e12
mathieuchebassier <mathieuchebassier@192.168.0.43>
2015-04-29 14:52:45 +0200

    Avec fenetre Rechercher un patient

--- src/Interface/Fenetre.java
+++ src/Interface/Fenetre.java
@@ -25,6 +25,7 @@ import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 import javax.swing.SwingConstants;
 import static javax.swing.SwingConstants.CENTER;
 
@@ -33,12 +34,23 @@ import static javax.swing.SwingConstants.CENTER;
  * @author mathieuchebassier
  */
 public class Fenetre extends JFrame{
-    final JButton rechercher, ajouter, admin, consulter, emp_presents, lit_libre, stats;
+   // final JButton rechercher, ajouter, admin, consulter, emp_presents, lit_libre, stats;
     public Fenetre()
     {
-    
+        this.setTitle("Gestion Hopital");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Quand on clique sur la croix, ça quitte proprement
+        this.setLayout(new FlowLayout()); // La fenêtre est répartie en grille : 8 lignes, 2 colonnes
 
- // Déclaration variables
+        fenetre_accueil();
+        //fenetre_rechercher_patient();
+    }
+    
+    
+    public void fenetre_accueil()
+    {
+        JButton rechercher, ajouter, admin, consulter, emp_presents, lit_libre, stats;        
+        
+        // Déclaration variables
         rechercher = new JButton("Rechercher un patient");
         ajouter = new JButton("Ajouter un patient");
         admin = new JButton("Admin");
@@ -59,11 +71,6 @@ public class Fenetre extends JFrame{
         lit_libre.setPreferredSize(new Dimension(400,30));
         stats.setPreferredSize(new Dimension(400,30));
         
-        // On remplit
-        this.setTitle("Gestion Hopital");
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Quand on clique sur la croix, ça quitte proprement
-        this.setLayout(new FlowLayout()); // La fenêtre est répartie en grille : 8 lignes, 2 colonnes
-
         pAdmin = new JPanel();
         pAdmin.setOpaque(false);;
         pAdmin.add(admin);
@@ -105,12 +112,58 @@ public class Fenetre extends JFrame{
         p7.setOpaque(false);
         
         // On gère les événements
-        if (rechercher.getModel().isPressed()) System.out.println("Recherche...");
+        // AJOUTER
         ajouter.addActionListener(new ActionListener()
         {
           public void actionPerformed(ActionEvent e)
           { 
             System.out.println("Ajouter");
+          }
+        });
+        
+        // RECHERCHER
+        rechercher.addActionListener(new ActionListener()
+        {
+          public void actionPerformed(ActionEvent e)
+          { 
+            
+                fenetre_rechercher_patient();
+          }
+        });
+        
+        //Admin
+        admin.addActionListener(new ActionListener()
+        {
+          public void actionPerformed(ActionEvent e)
+          { 
+            System.out.println("Admin...");
+          }
+        });
+        
+        //Consulter
+        consulter.addActionListener(new ActionListener()
+        {
+          public void actionPerformed(ActionEvent e)
+          { 
+            System.out.println("Consulter...");
+          }
+        });
+        
+        //Lit Libresif (rechercher.getModel().isPressed()) System.out.println("Recherche...");
+        lit_libre.addActionListener(new ActionListener()
+        {
+          public void actionPerformed(ActionEvent e)
+          { 
+            System.out.println("Liste lits libres...");
+          }
+        });
+        
+        // Stats
+        ajouter.addActionListener(new ActionListener()
+        {
+          public void actionPerformed(ActionEvent e)
+          { 
+            System.out.println("Données statistiques...");
           }
         });
         
@@ -126,15 +179,160 @@ public class Fenetre extends JFrame{
         this.add(p7);
         this.setSize(600,600);
         
-        this.setVisible(true);   
+        this.setVisible(true);  
     }
     
-    
-
-   // A terme j'aimerai faire ça
-    public void fenetre_accueil(JFrame f)
+    public void fenetre_rechercher_patient()
     {
-         
+        JTextField jtf_no_id, jtf_nom, jtf_prenom, jtf_no_chambre, jtf_no_lit, jtf_datea, jtf_adresse, jtf_tel, jtf_mutuelle;
+        JLabel jl_no_id, jl_nom, jl_prenom, jl_no_chambre, jl_no_lit, jl_datea, jl_adresse, jl_tel, jl_mutuelle, texte;
+        JButton valider = new JButton("Valider");
+        JButton retour = new JButton("Retour");
+        JPanel p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11;
+        
+        
+        // On initialise les JLabel
+        texte = new JLabel("Veuillez remplir les informations connues sur le patient");
+        jl_no_id = new JLabel("N° identification");
+        jl_nom = new JLabel("Nom");
+        jl_prenom = new JLabel("Prénom");
+        jl_no_chambre = new JLabel("N° chambre");
+        jl_no_lit = new JLabel("N° lit");
+        jl_datea = new JLabel("Date d'arrivée");
+        jl_adresse = new JLabel("Adresse");
+        jl_tel = new JLabel("N° telephone");
+        jl_mutuelle = new JLabel("Mutuelle");
+        
+        // On iitialise les JTF
+        jtf_no_id = new JTextField();
+        jtf_nom = new JTextField();
+        jtf_prenom = new JTextField();
+        jtf_no_chambre = new JTextField();
+        jtf_no_lit = new JTextField();
+        jtf_datea = new JTextField();
+        jtf_adresse = new JTextField();
+        jtf_tel = new JTextField();
+        jtf_mutuelle = new JTextField();
+        
+        jtf_no_id.setColumns(10);
+        jtf_nom.setColumns(10);
+        jtf_prenom.setColumns(10);
+        jtf_no_chambre.setColumns(10);
+        jtf_no_lit.setColumns(10);
+        jtf_datea.setColumns(10);
+        jtf_adresse.setColumns(10);
+        jtf_tel.setColumns(10);
+        jtf_mutuelle.setColumns(10);
+        
+        // On change le bouton de forme
+        valider.setPreferredSize(new Dimension(200,30));
+        valider.setOpaque(false);
+        retour.setPreferredSize(new Dimension(200,30));
+        retour.setOpaque(false);
+        
+        // On initialise les JPanel
+        p1 = new JPanel();
+        p1.add(texte);
+        p1.setOpaque(false);
+        p1.setPreferredSize(new Dimension(600, 100));
+        
+        p2 = new JPanel();
+        p2.add(jl_no_id);
+        p2.add(jtf_no_id);
+        p2.setOpaque(false);
+        p2.setPreferredSize(new Dimension(600, 30));
+        
+        p3 = new JPanel();
+        p3.add(jl_nom);
+        p3.add(jtf_nom);
+        p3.setOpaque(false);
+        p3.setPreferredSize(new Dimension(600, 30));
+        
+        p4 = new JPanel();
+        p4.add(jl_prenom);
+        p4.add(jtf_prenom);
+        p4.setOpaque(false);
+        p4.setPreferredSize(new Dimension(600, 30));
+        
+        p5 = new JPanel();
+        p5.add(jl_no_chambre);
+        p5.add(jtf_no_chambre);
+        p5.setOpaque(false);
+        p5.setPreferredSize(new Dimension(600, 30));
+        
+        p6 = new JPanel();
+        p6.add(jl_no_lit);
+        p6.add(jtf_no_lit);
+        p6.setOpaque(false);
+        p6.setPreferredSize(new Dimension(600, 30));
+        
+        p7 = new JPanel();
+        p7.add(jl_datea);
+        p7.add(jtf_datea);
+        p7.setOpaque(false);
+        p7.setPreferredSize(new Dimension(600, 30));
+        
+        p8 = new JPanel();
+        p8.add(jl_adresse);
+        p8.add(jtf_adresse);
+        p8.setOpaque(false);
+        p8.setPreferredSize(new Dimension(600, 30));
+        
+        p9 = new JPanel();
+        p9.add(jl_tel);
+        p9.add(jtf_tel);
+        p9.setOpaque(false);
+        p9.setPreferredSize(new Dimension(600, 30));
+        
+        p10 = new JPanel();
+        p10.add(jl_mutuelle);
+        p10.add(jtf_mutuelle);
+        p10.setOpaque(false);
+        p10.setPreferredSize(new Dimension(600, 30));
+        
+        p11 = new JPanel();
+        p11.add(retour);
+        p11.add(valider);
+        p11.setOpaque(false);
+        p11.setPreferredSize(new Dimension(600, 30));
+        
+        // On gère l'évennement du bouton
+        valider.addActionListener(new ActionListener()
+        {
+          public void actionPerformed(ActionEvent e)
+          { 
+            System.out.println("Il valide avec :");
+            System.out.println("N° id : "+jtf_no_id.getText());
+            System.out.println("Nom : "+jtf_nom.getText()); // ETC
+          }
+        });
+        
+        retour.addActionListener(new ActionListener()
+        {
+          public void actionPerformed(ActionEvent e)
+          { 
+            fenetre_accueil();
+          }
+        });
+        
+        // On ajoute tous les JPannel à la fenêtre
+        this.setContentPane(new ImagePanel(new ImageIcon("fond66.jpg").getImage())); // Met l'image en background
+        this.add(p1);
+        this.add(p2);
+        this.add(p3);
+        this.add(p4);
+        this.add(p5);
+        this.add(p6);
+        this.add(p7);
+        this.add(p8);
+        this.add(p9);
+        this.add(p10);
+        this.add(p11);
+        
+        this.setSize(600,600);
+        
+        this.setVisible(true); 
+        
     }
 }
 
